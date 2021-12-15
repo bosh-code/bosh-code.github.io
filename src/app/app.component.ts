e9fdd8a736ece811460c8de97d31bba67f184d71
ryanbosher <ryan.bosher@lancom.tech>
2021-12-15 14:23:45 +1300

    add cheeky log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
@@ -8,9 +8,13 @@ import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bosher.co.nz';
   faLinkedIn = faLinkedin;
   faGitHub = faGithub;
   faEmail = faEnvelope;
+
+  ngOnInit(): void {
+    console.log("%cNothing to see here","color: red; font-size: 20px");
+  }
 }
